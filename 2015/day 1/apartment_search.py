fff5048928b38f1fe6c9b8b97475e82606b55b54
Aidan Johnson <johnsoac671@gmail.com>
2024-10-29 14:08:07 -0700

    day 1 - 2015

--- 2015/day 1/apartment_search.py
+++ 2015/day 1/apartment_search.py
@@ -1,2 +1,20 @@
+from itertools import accumulate
+
 def find_apartment(string):
-    return sum(map(lambda x: 1 if x == "(" else ")", string))
+    return sum(map(lambda x: 1 if x == "(" else -1, string))
+
+
+def enter_basement(string):
+    running_totals = list(accumulate(map(lambda x: 1 if x == "(" else -1, string)))
+    
+    return running_totals.index(-1) + 1 if -1 in running_totals else None
+     
+
+
+def test():
+    with open(r"2015\day 1\input.txt", "r") as file:
+        string = file.read()
+        print(f"Santa needs to go to apartment: {find_apartment(string)}")
+        print(f"Santa enters the basement on move: {enter_basement(string)}")
+
+test()
